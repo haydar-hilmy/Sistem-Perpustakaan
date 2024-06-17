34d2efe3b1339f3a24aacf6768857c21f4b6fd83
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-06-17 20:41:06 +0700

    ganti judul

--- src/main/java/com/fxml/pbo/pbofxml/HelloApplication.java
+++ src/main/java/com/fxml/pbo/pbofxml/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 420);
-        stage.setTitle("Hello!");
+        stage.setTitle("Minim Library");
         stage.setScene(scene);
         stage.show();
     }
