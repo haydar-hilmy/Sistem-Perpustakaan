6e85273a7ea46cf9fb699c9f52fdc10ee0be0ad6
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-06-19 12:17:23 +0700

    gk bisa tampil data, config controller n initialize,loadData

--- src/main/java/com/fxml/pbo/pbofxml/AppController.java
+++ src/main/java/com/fxml/pbo/pbofxml/AppController.java
@@ -1,14 +1,54 @@
 package com.fxml.pbo.pbofxml;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class AppController {
+public class AppController implements Initializable {
+    ObservableList<Anggota> listAnggota = FXCollections.observableArrayList() ;
     @FXML
-    private Label welcomeText;
+    private TableView<Anggota> tvAnggota;
+    @FXML
+    private TableColumn<Anggota, Integer> tcIdAnggota;
+    @FXML
+    private TableColumn<Anggota, String> tcNamaAnggota;
+    @FXML
+    private TableColumn<Anggota, String> tcAlamatAnggota;
+    @FXML
+    private TableColumn<Anggota, String> tcInstansiAnggota;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initTableAnggota();
+        loadData();
+//        setFilter();
+//        setButton(true,true,true,false,false);
+//        setTeks(false);
+//        setFilter();
     }
+    void initTableAnggota(){
+        tcIdAnggota.setCellValueFactory(new PropertyValueFactory<Anggota,Integer>("idAnggota"));
+        tcNamaAnggota.setCellValueFactory(new PropertyValueFactory<Anggota,String>("nama"));
+        tcAlamatAnggota.setCellValueFactory(new PropertyValueFactory<Anggota,String>("penerbit"));
+        tcInstansiAnggota.setCellValueFactory(new PropertyValueFactory<Anggota,String>("instansi"));
+    }
+
+    void loadData(){
+        listAnggota = DBUtil.getDataAnggota();
+        tvAnggota.setItems(listAnggota);
+    }
+
 }
