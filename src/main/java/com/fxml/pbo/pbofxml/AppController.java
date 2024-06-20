e3364b641c075bd8e10980dfca3f3517e708f7e4
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-06-20 23:49:17 +0700

    CRUD Anggota WELL DONE

--- src/main/java/com/fxml/pbo/pbofxml/AppController.java
+++ src/main/java/com/fxml/pbo/pbofxml/AppController.java
@@ -9,13 +9,13 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class AppController implements Initializable {
     ObservableList<Anggota> listAnggota = FXCollections.observableArrayList();
@@ -30,8 +30,105 @@ public class AppController implements Initializable {
     @FXML
     private TableColumn<Anggota, String> tcInstansiAnggota;
 
+
+//    TAB ANGGOTA
+    @FXML
+    private TabPane tabPaneAnggota;
+    @FXML
+    private Tab tabAnggotaForm;
+    @FXML
+    private Tab tabAnggotaFormEdit;
+    @FXML
+    private Tab tabAnggotaTableView;
+    @FXML
+    private Button btnEditAnggota;
+    @FXML
+    private Button btnSubmitAnggota;
+    @FXML
+    private Button btnSubmitAnggotaEdit;
+    @FXML
+    private Button btnCancelAnggota;
+    @FXML
+    private Button btnCancelAnggotaEdit;
+
+    // TEXTFIELD ANGGOTA
+    @FXML
+    private TextField tfIdAnggotaEdit, tfNamaAnggotaEdit, tfAlamatAnggotaEdit, tfInstansiAnggotaEdit;
+    @FXML
+    private TextField tfNamaAnggotaAdd, tfAlamatAnggotaAdd, tfInstansiAnggotaAdd;
+
+    @FXML
+    void chooseAnggota(MouseEvent event) {
+            Anggota ag = tvAnggota.getSelectionModel().getSelectedItem();
+        if(ag != null){
+            tfIdAnggotaEdit.setText(String.valueOf(ag.getIdAnggota()));
+            tfNamaAnggotaEdit.setText(ag.getNama());
+            tfAlamatAnggotaEdit.setText(ag.getAlamat());
+            tfInstansiAnggotaEdit.setText(ag.getInstansi());
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        btnEditAnggota.setOnAction(event -> {
+            tabPaneAnggota.getSelectionModel().select(tabAnggotaFormEdit);
+            setTfEditAnggota(true, false);
+            tfNamaAnggotaEdit.requestFocus();
+            System.out.println("Moved -> Edit Data");
+        });
+
+        btnSubmitAnggotaEdit.setOnAction(event -> {
+            Connection conn = DBConnection.getConn();
+            String sql = "UPDATE anggota SET nama = ?, alamat = ?, instansi = ? WHERE id_anggota = ?";
+            try {
+                PreparedStatement st = conn.prepareStatement(sql);
+                st.setString(1, tfNamaAnggotaEdit.getText());
+                st.setString(2, tfAlamatAnggotaEdit.getText());
+                st.setString(3, tfInstansiAnggotaEdit.getText());
+                st.setInt(4, Integer.valueOf(tfIdAnggotaEdit.getText()));
+                st.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            loadDataAnggota();
+            setTfEditAnggota(false, true);
+            System.out.println("Edited Data");
+            tabPaneAnggota.getSelectionModel().select(tabAnggotaTableView);
+            System.out.println("Moved -> Table View Anggota");
+        });
+
+        btnCancelAnggotaEdit.setOnAction(event -> {
+            setTfEditAnggota(false, true);
+            System.out.println("Cancel Edit");
+        });
+
+        btnSubmitAnggota.setOnAction(event -> {
+            Connection conn = DBConnection.getConn();
+            String sql = "INSERT INTO anggota (nama, alamat, instansi) VALUES (?, ?, ?)";
+            try {
+                PreparedStatement st = conn.prepareStatement(sql);
+                st.setString(1, tfNamaAnggotaAdd.getText());
+                st.setString(2, tfAlamatAnggotaAdd.getText());
+                st.setString(3, tfInstansiAnggotaAdd.getText());
+                st.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            setTfAddAnggota(true, true);
+            loadDataAnggota();
+            System.out.println("Added Anggota");
+            tabPaneAnggota.getSelectionModel().select(tabAnggotaTableView);
+            System.out.println("Moved -> Table View Anggota");
+        });
+
+        btnCancelAnggota.setOnAction(event -> {
+            // bersihkan textfield
+            System.out.println("Reset Form");
+            setTfAddAnggota(true, true);
+        });
+
         initTableAnggota();
         loadDataAnggota();
 //        setFilter();
@@ -44,6 +141,50 @@ public class AppController implements Initializable {
         tcNamaAnggota.setCellValueFactory(new PropertyValueFactory<Anggota, String>("nama"));
         tcAlamatAnggota.setCellValueFactory(new PropertyValueFactory<Anggota, String>("alamat"));
         tcInstansiAnggota.setCellValueFactory(new PropertyValueFactory<Anggota, String>("instansi"));
+    }
+
+    void setTfEditAnggota(Boolean isActive, Boolean clear){
+        if(clear == true){
+            tfIdAnggotaEdit.setText(null);
+            tfNamaAnggotaEdit.setText(null);
+            tfAlamatAnggotaEdit.setText(null);
+            tfInstansiAnggotaEdit.setText(null);
+        }
+        tfIdAnggotaEdit.setEditable(false);
+        tfNamaAnggotaEdit.setEditable(isActive);
+        tfAlamatAnggotaEdit.setEditable(isActive);
+        tfInstansiAnggotaEdit.setEditable(isActive);
+    }
+
+    void setTfAddAnggota(Boolean isActive, Boolean clear){
+        if(clear == true){
+            tfNamaAnggotaAdd.setText(null);
+            tfAlamatAnggotaAdd.setText(null);
+            tfInstansiAnggotaAdd.setText(null);
+        }
+        tfNamaAnggotaAdd.setEditable(isActive);
+        tfAlamatAnggotaAdd.setEditable(isActive);
+        tfInstansiAnggotaAdd.setEditable(isActive);
+    }
+
+    @FXML
+    void deleteAnggota(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete Data?", ButtonType.YES, ButtonType.CANCEL);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.YES) {
+            Connection conn = DBConnection.getConn();
+            String sql = "DELETE FROM anggota WHERE id_anggota = ?";
+            PreparedStatement st = null;
+            try {
+                st = conn.prepareStatement(sql);
+                st.setString(1, tfIdAnggotaEdit.getText());
+                st.executeUpdate();
+                loadDataAnggota();
+                setTfEditAnggota(false, true);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     void loadDataAnggota(){
